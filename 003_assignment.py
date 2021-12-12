4af43886b48a811beeeb68ddf962e6c3955dd194
anant533 <anant533@gmail.com>
2021-12-12 15:01:11 -0500

    Initial commit

---
+++ 003_assignment.py
@@ -0,0 +1,7 @@
+print('hello, I am "Anant".')
+print('''hello, I'am "Anant".''')
+
+print("I am \"Anant\".")
+
+print("Hello, I'm \"Anant\".")
+
---
+++ 005_assignment02.py
@@ -0,0 +1,9 @@
+x=input("input the side perpendicular length: ")
+a_leg = float (x)
+
+y=input("input the side base length: ")
+b_leg = float (y)
+
+z=((a_leg**2+b_leg**2)**0.5)
+print(z)
+
