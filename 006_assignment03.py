e576463067fb62377dd4fbfe06ad58af5512d3cc
anant533 <anant533@gmail.com>
2021-12-15 15:11:10 -0500

    Update 006_assignment03.py

--- 006_assignment03.py
+++ 006_assignment03.py
@@ -1,3 +1,12 @@
 print(796/60)
 print(796//60)
 print(796%60)
+
+
+H1=int(input("Start time in hours: "))
+M1=int(input("start time in minutes: "))
+d=int(input("Time duration in minutes: "))
+F=H1*60+M1+d
+H2=F//60
+M2=F-H2*60
+print("The finish time is = ",H2,":",M2)
